70156b4f8a3439aed3e2e65ccf43f5e2496e5c38
Michael Wettstein <chischte@gmx.ch>
2020-08-08 16:15:06 +0200

    Chapter 78 add 404 page and switch

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import "./styles/styles.scss";
 
 import React from "react";
 import ReactDOM, { render } from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route,Switch } from "react-router-dom";
 
 const ExpenseDashboardPage = () => (
   <div>This is from my dashboard component</div>
@@ -12,15 +12,17 @@ const ExpenseDashboardPage = () => (
 const AddExpensePage = () => <div>AddExpensePage</div>;
 const EditExpensePage = () => <div>EditExpensePage</div>;
 const HelpPage = () => <div>HelpPage</div>;
+const NotFound = () => <div>404!</div>;
 
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
     <Route path="/" component={ExpenseDashboardPage} exact={true} />
+    <Route path="/create" component={AddExpensePage} />
     <Route path="/help" component={HelpPage} />
     <Route path="/edit" component={EditExpensePage} />
-    <Route path="/create" component={AddExpensePage} />
-    </div>
+    <Route component={NotFound} />
+    </Switch>
   </BrowserRouter>
 );
 
