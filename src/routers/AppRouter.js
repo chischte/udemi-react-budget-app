d822c70af243c31a32844feb4f21e3ef546ee27c
Michael Wettstein <chischte@gmx.ch>
2020-08-08 21:36:49 +0200

    Chapter 80 refactoring

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import ExpenseDashboardPage from "../components/ExpenseDashboard";
+import Header from "../components/Header";
+import AddExpensePage from "../components/AddExpense";
+import EditExpensePage from "../components/EditExpense";
+import HelpPage from "../components/HelpPage";
+import NotFound from "../components/NotFound";
+
+const AppRouter = () => (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+export default AppRouter;
